a21da2806d52756b4571e2fae9343a0a60512a3e
Blaza-4 <adinnavakumar22@gmail.com>
2025-06-13 11:40:22 +0530

    included headers and functions pros

---
+++ headers.h
@@ -0,0 +1,15 @@
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int sum(int a,int b);
+int diff(int a,int b);
+#ifdef __cplusplus
+}
+#endif
