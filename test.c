6080fc56ee2568a4c4f9531e1d2e58bdeff78458
Blaza-4 <adinnavakumar22@gmail.com>
2025-06-13 11:42:16 +0530

    code

--- test.c
+++ test.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include "headers.h"
 #define ERR_BIT 0
 #define RANGE_BIT 1
 void check(int nNumber);
-/*
- @brief function to validate bits of a number  
- @param function parameters include an integer number   
+/** 
+ *@brief function to validate bits of a number  
+ *@param function parameters include an integer number   
 */
-int main (){
-    int nNumber;
-    printf("enter number :");
-    scanf("%d",&nNumber);
-    if(nNumber <= 0){
-        return 1;
-    } else {
-        check(nNumber);
+int sum(int a, int b){
+    if(a < 0 || b < 0){
+        return 0;
     }
+    return a+b;
+}
+int diff(int a , int b)
+{
+    return a-b ;
 }
 void check(int nNumber){
      if(nNumber & (1<<ERR_BIT))        //checking the first bit of the byte
