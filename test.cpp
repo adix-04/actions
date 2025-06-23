8b878de6c97587a7a50a37b8637e2b6aab567105
Blaza-4 <adinnavakumar22@gmail.com>
2025-06-23 22:21:39 +0530

    chore :changes

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ TEST(sample_test, first_sum){
     EXPECT_EQ(sum(-1,-2),0);
 }
 TEST(sample_test2, diff_test){
-    EXPECT_EQ(diff(20,10),11);
+    EXPECT_EQ(diff(20,10),10);
     EXPECT_EQ(diff(13,2),11);
     EXPECT_EQ(diff(22,10),12);
 }
